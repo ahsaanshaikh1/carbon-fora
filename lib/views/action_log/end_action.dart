2a55027e6197c890f11dc5031e26379cab9a2b94
theodorascott108 <theodorascott108@gmail.com>
2025-08-16 13:49:53 +0500

    commit

--- lib/views/action_log/end_action.dart
+++ lib/views/action_log/end_action.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:carbon_fora/route_structure/go_navigator.dart';
 import 'package:carbon_fora/theme/colors.dart';
 import 'package:carbon_fora/theme/font_structures.dart';
@@ -6,6 +9,7 @@ import 'package:carbon_fora/views/action_log/action_submitted.dart';
 import 'package:carbon_fora/widgets/custom_button.dart';
 import 'package:carbon_fora/widgets/filled_box.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class EndAction extends StatefulWidget {
   const EndAction({super.key});
@@ -15,6 +19,57 @@ class EndAction extends StatefulWidget {
 }
 
 class _EndActionState extends State<EndAction> {
+  final DateTime startTime = DateTime.now();
+  late Timer _timer;
+  late Duration _elapsed;
+  StreamSubscription<Position>? positionStream;
+  @override
+  void initState() {
+    super.initState();
+
+    positionStream =
+        Geolocator.getPositionStream(
+          locationSettings: const LocationSettings(
+            accuracy: LocationAccuracy.bestForNavigation,
+            // timeLimit: Duration(seconds: 1), // meters
+          ),
+        ).listen((Position position) {
+          log(position.speed.toString());
+        });
+    _elapsed = DateTime.now().difference(startTime);
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (mounted) {
+        setState(() {
+          _elapsed = DateTime.now().difference(startTime);
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel subscription to avoid memory leaks
+    if (mounted) {
+      positionStream?.cancel();
+    }
+    super.dispose();
+  }
+
+  String _formatElapsed(Duration elapsed) {
+    if (elapsed.inSeconds < 60) {
+      return "${elapsed.inSeconds} secs";
+    } else if (elapsed.inMinutes < 60) {
+      return "${elapsed.inMinutes} mins";
+    } else if (elapsed.inHours < 24) {
+      return "${elapsed.inHours} hours";
+    } else if (elapsed.inDays < 30) {
+      return "${elapsed.inDays} days";
+    } else {
+      final months = (elapsed.inDays / 30).floor();
+      return "$months months";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -106,7 +161,57 @@ class _EndActionState extends State<EndAction> {
                 ),
               ),
               60.kH,
-              Image.asset("assets/images/png/timer.png"),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white.withOpacity(.15),
+                  border: Border.all(
+                    color: Colors.white.withOpacity(.25),
+                    width: 1,
+                  ),
+                ),
+                height: 263,
+                width: 263,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/images/png/stopwatch.png",
+                            height: 26,
+                            fit: BoxFit.cover,
+                            width: 26,
+                          ),
+                          5.kW,
+                          Text(
+                            "Timer",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      5.kH,
+                      Text(
+                        _formatElapsed(_elapsed),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Image.asset("assets/images/png/timer.png"),
               60.kH,
               Text(
                 "GPS Tracking",
